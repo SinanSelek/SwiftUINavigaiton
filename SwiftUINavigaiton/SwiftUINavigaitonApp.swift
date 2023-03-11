e9f1f943ec3a7b6f5b9c30e2445a60a23fb89c6f
Sinan Selek <sselek62@gmail.com>
2023-03-11 23:54:59 +0300

    Initial Commit

---
+++ SwiftUINavigaiton/SwiftUINavigaitonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigaitonApp.swift
+//  SwiftUINavigaiton
+//
+//  Created by Sinan Selek on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigaitonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
